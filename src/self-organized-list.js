f39d9086024adea7ddc6c0d8934610cedd3dff2c
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-02-20 01:27:17 +0300

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -13,46 +13,128 @@ class SelfOrganizedList {
     }
 
     insert(data) {
-      
+
+        var qwe = new Node(data);
+        if (this.head == null)
         {
-            sol.insert(null);
-        };
-        {
-        sol.insert('Pike');
-        sol.insert('Bass');
-        sol.insert('Roach');
+            this.head = qwe;
+            this.tail = qwe;
+
+        } else {
+            this.tail.next = qwe;
+            qwe.next = null;
+            qwe.prev = this.tail;
+            this.tail = qwe;
         }
     }
 
     size() {
-        sol.size()
+        if (this.head == null)
+        {
+            return 0;
+        }
+        var i = 1;
+         var val = this.head;
+
+        while (val.next != null)
+        {
+            val = val.next;
+            i++;
+        }
+        return i;
     }
 
     at(index) {
-        sol.at(1);
-        sol.at(2);
-        sol.at(0);
+        var arr2 = this.toArray();
+               if ( index >= arr2.length || arr2.length === 0)
+                {
+                   return null;
+               }
+               else
+               {
+               return arr2[index];
     }
-
+}
     findNode(data) {
-        sol.findNode('Pike');
+
+        if (!this.size())
+        {
+            return null;
+        }
+        var node = this.head;
+        while (node != null)
+        {
+            if (node.data === data)
+            {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
     }
 
     toArray() {
-        sol.toArray();
+       if (!this.size())
+       {
+                  return [];
+              }
+              let arr = [];
+              var node = this.head;
+              while (node)
+               {
+                  arr.push(node.data);
+                  node = node.next;
+              }
+              return arr;
     }
 
     removeAt(index) {
-        sol.removeAt(2)
+       console.log('==================this')
+       console.log(this)
+       console.log('====================index')
+       console.log(index)
     }
-
     moveToFront(node) {
-        sol.moveToFront('Roach')
-    }
+        if(!this.size())
+        {
+                    return 0;
+        }
 
-    reorganize(data) {
-        sol.reorganize('Bass');
+                var node2 = this.head;
+                while(node2 != null){
+                    if(node2 == node)
+                    {
+                        while(node2 != this.head)
+                        {
+                            var qwe = node2.data;
+                            node2.data = node2.prev.data;
+                            node2.prev.data = qwe;
+                            node2 = node2.prev;
+                        }
+                        return 0;
+                    }
+                    node2 = node2.next;
+                }
     }
+    reorganize(data) {
+        if(!this.size())
+        {
+                    return false;
+
+        }
+                var node =this.head;
+                for(var i=0;i<this.size();i++)
+                {
+                    if(node.data==data)
+                    {
+                        this.moveToFront(node);
+                        return true;
+                    }
+                    node=node.next;
+                }
+
+                return false;
+            }
 
 }
 
